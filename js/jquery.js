63d7abb63c50f215777a0d988cdc41e83903e5e5
Wemdavis <fasunonbukola@gmail.com>
2023-05-18 16:10:25 +0100

    Readjusted the design to fit the user story given.

--- js/jquery.js
+++ js/jquery.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
-    // Define a function to retrieve a random quote
-    function fetchQuote() {
-      $.ajax({
-        url: 'https://api.quotable.io/random',
-        dataType: 'json',
-        success: function(data) {
-          $('#new-quote').text(data.content);
-          $('#quote-author').text('- ' + data.author);
-        },
-        error: function() {
-          alert('An error occurred while retrieving the quote.');
-        }
-      });
-    }
-  
-    // Call the fetchQuote function on document load to display an initial quote
+  // Define a function to retrieve a random quote
+  function fetchQuote() {
+    $.ajax({
+      url: 'https://api.quotable.io/random',
+      dataType: 'json',
+      success: function(data) {
+        $('#text').text(data.content);
+        $('#author').text('- ' + data.author);
+        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + encodeURIComponent('"' + data.content + '" ' + data.author));
+      },
+      error: function() {
+        alert('An error occurred while retrieving the quote.');
+      }
+    });
+  }
+
+  // Call the fetchQuote function on document load to display an initial quote
+  fetchQuote();
+
+  // Set up a click event listener for the new-quote button
+  $('#new-quote').on('click', function() {
     fetchQuote();
-  
-    // Set a timer to fetch a new quote every 10 seconds
-    setInterval(fetchQuote, 10000);
   });
+});
